cf8a571790da75d17bb5e528406a49d143304bd7
n3f <b@n3f.co>
2020-09-26 17:26:48 -0700

    Add version command line argument.

--- filecasted/cli.py
+++ filecasted/cli.py
@@ -1,5 +1,17 @@
 import click
+import os
+
+from .project import get_version
+
+
+def print_version(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    click.echo(get_version())
+    ctx.exit()
+
 
 @click.command()
+@click.option('--version', expose_value=False, is_eager=True, callback=print_version, is_flag=True)
 def run():
     click.echo('yo')
---
+++ filecasted/project.py
@@ -0,0 +1,18 @@
+import addict
+import pathlib
+import toml
+
+
+def load_config():
+    config = None
+    file = pathlib.Path(__file__) / '../../pyproject.toml'
+    file = file.resolve()
+    with file.open() as f:
+        config = toml.load(f)
+    return addict.Dict(config)
+
+
+def get_version():
+    """Get the project version from the poetry file"""
+    config = load_config()
+    return f'{config.tool.poetry.name}: {config.tool.poetry.version}'
