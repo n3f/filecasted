21e6ebbf42254725a8eebf1c51b96729fafb1578
n3f <b@n3f.co>
2020-09-26 19:56:23 -0700

    Initial parameters.

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# filecasted
-An RSS editor to create/modify a podcast with files.
+# Filecasted
+
+Create or modify podcast xml files using local files (i.e. `m3u`, `mp3`, `mp4`, etc.)
+
+## Usage
+
+Consists of several commands (`create`, `edit`). Use the `--help` option for additional information.
+
+`$ filecast create --help`
+
+```shell
+Usage: filecast [OPTIONS] INPUT
+
+  Create a podcast file from INPUT.
+
+  INPUT can be a playlist (`.m3u`), stdin (`-`), or a collection of audio
+  files.  Playlists and stdin should be a simple text document with a list
+  of files to add.
+
+Options:
+  --version
+  -f, --force        Force the output to an existing file.
+  -a, --append       Append INPUT to the output file.
+  -o, --output PATH
+  --help             Show this message and exit.
+```
+
+## Examples
+
+Create a podcast from a directory of audio files.
+
+`$ filecast create`
--- filecasted/cli.py
+++ filecasted/cli.py
@@ -1,5 +1,5 @@
 import click
-import os
+import pathlib
 
 from .project import get_version
 
@@ -11,7 +11,27 @@ def print_version(ctx, param, value):
     ctx.exit()
 
 
+def validate_output(ctx, param, value):
+    output = pathlib.Path(value).resolve()
+    if (output.exists() and (
+            ctx.params.get('force', False) is not True or
+            ctx.params.get('append', False) is not True)):
+        raise click.BadParameter('file exists')
+
+
 @click.command()
 @click.option('--version', expose_value=False, is_eager=True, callback=print_version, is_flag=True)
-def run():
-    click.echo('yo')
+@click.option('-f', '--force', is_flag=True, is_eager=True, help='Force the output to an existing file.')
+@click.option('-a', '--append', is_flag=True, is_eager=True, help='Append INPUT to the output file.')
+@click.option('-o', '--output', nargs=1, type=click.Path(exists=False, writable=True, allow_dash=True),
+              default='./rss.xml', callback=validate_output)
+@click.argument('_input', nargs=-1, type=click.Path(exists=True, readable=True, allow_dash=True), metavar='INPUT')
+def run(**kwargs):
+    """
+    Create a podcast file from INPUT.
+
+    INPUT can be a playlist (`.m3u`), stdin (`-`), or a collection of audio files.  Playlists and stdin should
+    be a simple text document with a list of files to add.
+    """
+    click.echo(f'{kwargs.get("_input")}:{kwargs.get("output")}')
+
